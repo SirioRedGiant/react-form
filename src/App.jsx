075140b434efb62ec2062087440ef47b63339015
SirioRedGiant <Oiris1992@gmail.com>
2026-02-10 16:57:29 +0100

    Aggiunta la possibilità di aggiungere un articolo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/index.css";
+import { useState } from "react";
 
 export default function App() {
-  const articles = [
+  const [articles, setArticles] = useState([
     { id: "1", title: "Sparatutto" },
     { id: "2", title: "Avventura" },
     { id: "3", title: "Real Time Strategies" },
-  ];
+  ]);
+
+  const [formData, setFormData] = useState("");
+
+  //note Funzione per gestire l'aggiunta
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const maxId =
+      articles.length > 0
+        ? Math.max(...articles.map((a) => parseInt(a.id)))
+        : 0;
+
+    const newArticle = {
+      id: (maxId + 1).toString(),
+      title: formData,
+    };
+
+    setArticles([...articles, newArticle]);
+    setFormData("");
+  };
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
@@ -15,13 +36,34 @@ export default function App() {
             Articoli
           </h2>
 
+          {/*//^   FORM   */}
+          <form
+            onSubmit={handleSubmit}
+            className="mb-5 shadow-sm p-4 bg-light rounded border"
+          >
+            <label className="form-label fw-bold">Nuovo Articolo</label>
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Inserisci il titolo..."
+                value={formData}
+                onChange={(e) => setFormData(e.target.value)}
+              />
+              <button className="btn btn-primary px-4" type="submit">
+                Aggiungi
+              </button>
+            </div>
+          </form>
+
+          {/*//^ LISTA ARTICOLI */}
           <ul className="list-group shadow-lg">
             {articles.map((article) => (
               <li
                 key={article.id}
-                className="list-group-item list-group-item-action "
+                className="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
               >
-                {article.title}
+                <span className="fs-5">{article.title}</span>
               </li>
             ))}
           </ul>
